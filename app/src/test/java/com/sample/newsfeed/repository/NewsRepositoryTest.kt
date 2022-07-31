442eb72466dad8f3aa64b01d7c32d09538074d6e
Navas <navas.pk@avrioc.com>
2022-07-31 17:58:27 +0400

    Mockk added for macking usecase
    Signed-off-by: Navas <navas.pk@avrioc.com>

--- app/src/test/java/com/sample/newsfeed/repository/NewsRepositoryTest.kt
+++ app/src/test/java/com/sample/newsfeed/repository/NewsRepositoryTest.kt
@@ -5,21 +5,33 @@ import com.google.common.truth.Truth
 import com.sample.newsfeed.layers.data.api.NewsServiceApi
 import com.sample.newsfeed.layers.data.repository.NewsRepository
 import com.sample.newsfeed.layers.data.repository.NewsRepositoryImpl
+import com.sample.newsfeed.layers.domain.model.ItemsResponse
 import com.sample.newsfeed.layers.domain.usecase.NewsFeedUseCase
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 import org.mockito.Mockito.*
+import retrofit2.Response
 
-//@RunWith(JUnit4::class)
+@RunWith(JUnit4::class)
 class NewsRepositoryTest {
 
     private lateinit var repository: NewsRepository
     private val service = mock(NewsServiceApi::class.java)
+
+    @MockK
     private lateinit var useCase: NewsFeedUseCase
+
+    @MockK
+    private lateinit var responseOfItemsResponse : Response<ItemsResponse>
 
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
@@ -28,13 +40,18 @@ class NewsRepositoryTest {
 
     @Before
     fun init() {
+        MockKAnnotations.init(this)
         repository = NewsRepositoryImpl(service)
-        useCase = NewsFeedUseCase(repository)
+       // useCase = NewsFeedUseCase(repository)
+
+
     }
 
     @Test
     fun loadNewsFromNetwork() {
         runBlocking {
+            coEvery { useCase.createUseCase(null) } returns responseOfItemsResponse
+
             val data = useCase.createUseCase(null)
             Truth.assertThat(data).isNotNull()
         }
