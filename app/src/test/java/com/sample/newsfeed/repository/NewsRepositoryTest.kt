200efbceeb5d01b9d78d1e3e24af7efd374c73a2
Navas <navas.pk@avrioc.com>
2022-07-31 18:25:52 +0400

    Fixed error in testing with mockk
    Signed-off-by: Navas <navas.pk@avrioc.com>

--- app/src/test/java/com/sample/newsfeed/repository/NewsRepositoryTest.kt
+++ app/src/test/java/com/sample/newsfeed/repository/NewsRepositoryTest.kt
@@ -5,6 +5,7 @@ import com.google.common.truth.Truth
 import com.sample.newsfeed.layers.data.api.NewsServiceApi
 import com.sample.newsfeed.layers.data.repository.NewsRepository
 import com.sample.newsfeed.layers.data.repository.NewsRepositoryImpl
+import com.sample.newsfeed.layers.domain.model.ItemsResponse
 import com.sample.newsfeed.layers.domain.usecase.NewsFeedUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -12,14 +13,24 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
 import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Response
 
-//@RunWith(JUnit4::class)
+@RunWith(MockitoJUnitRunner::class)
 class NewsRepositoryTest {
 
     private lateinit var repository: NewsRepository
     private val service = mock(NewsServiceApi::class.java)
+
+    @Mock
     private lateinit var useCase: NewsFeedUseCase
+
+    @Mock
+    private lateinit var responseOfItemsResponse : Response<ItemsResponse>
 
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
@@ -28,13 +39,18 @@ class NewsRepositoryTest {
 
     @Before
     fun init() {
+
         repository = NewsRepositoryImpl(service)
-        useCase = NewsFeedUseCase(repository)
+       // useCase = NewsFeedUseCase(repository)
+
+
     }
 
     @Test
     fun loadNewsFromNetwork() {
         runBlocking {
+            `when`(useCase.createUseCase(null)).thenReturn(responseOfItemsResponse)
+
             val data = useCase.createUseCase(null)
             Truth.assertThat(data).isNotNull()
         }
